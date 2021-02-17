3ea32f6b8f5ae8bb0051952ddaf938b1b5508de1
ezikechris <christophermbe1009@gmail.com>
2021-02-17 17:21:52 +0100

    initial Commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,8 +19,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Nordic Giant</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; Copyright 2021, CodeMythology</li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">CodeMythology</a></li>
            </ul>
 
         </div>
